57056c77a6dbf5195e5d89be5a76f5964ef9e62e
github yamato0142 <yamato.0142tech@gmail.com>
2024-10-25 22:27:59 +0900

    TODOエリアのCSSを追加

--- src/components/Todo/CompleteTodo.jsx
+++ src/components/Todo/CompleteTodo.jsx
@@ -1,19 +1,62 @@
+import styled from "styled-components";
+
+const CompleteArea = styled.div`
+    border: 2px solid #aacfd0;
+    width: 400px;
+    min-height: 200px;
+    padding: 8px;
+    margin: 8px;
+    border-radius: 8px;
+    background-color: #c9dede;
+`;
+
+const Title = styled.p`
+    text-align: center;
+    margin-top: 0px;
+    font-weight: bold;
+`;
+
+const ListRow = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const TodoItem = styled.p`
+    margin: 6px;
+`;
+
+const Button = styled.button`
+    border-radius: 8px;
+    border: none;
+    padding: 4px 16px;
+    margin: 0px 3px;
+
+    &:hover{
+        background-color: #79a8a9;
+        color: #fff;
+        cursor: pointer;
+    }
+`;
 
 
 export const CompleteTodo = () => {
     return (
-        <>
-            <p>完了のTODO</p>
+        <CompleteArea>
+            <Title>完了のTODO</Title>
             <ul>
                 <li>
-                    <p>TODOでした</p>
-                    <button>戻す</button>
+                    <ListRow>
+                        <TodoItem>TODOでした</TodoItem>
+                        <Button>戻す</Button>
+                    </ListRow>
                 </li>
                 <li>
-                    <p>TODOでした</p>
-                    <button>戻す</button>
+                    <ListRow>
+                        <TodoItem>TODOでした</TodoItem>
+                        <Button>戻す</Button>
+                    </ListRow>
                 </li>
             </ul>
-        </>
+        </CompleteArea>
     )
 }
--- src/components/Todo/IncompleteTodo.jsx
+++ src/components/Todo/IncompleteTodo.jsx
@@ -1,21 +1,63 @@
+import styled from "styled-components";
+
+const IncompleteArea = styled.div`
+    border: 2px solid #aacfd0;
+    width: 400px;
+    min-height: 200px;
+    padding: 8px;
+    margin: 8px;
+    border-radius: 8px;
+`;
+
+const Title = styled.p`
+    text-align: center;
+    margin-top: 0px;
+    font-weight: bold;
+`;
+
+const ListRow = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const TodoItem = styled.p`
+    margin: 6px;
+`;
+
+const Button = styled.button`
+    border-radius: 8px;
+    border: none;
+    padding: 4px 16px;
+    margin: 0px 3px;
+
+    &:hover{
+        background-color: #79a8a9;
+        color: #fff;
+        cursor: pointer;
+    }
+`;
 
 
 export const InCompleteTodo = () => {
     return (
-        <>
-            <p>未完了のTODO</p>
+        <IncompleteArea>
+            <Title>未完了のTODO</Title>
             <ul>
                 <li>
-                    <p>TODOです</p>
-                    <button>完了</button>
-                    <button>削除</button>
+                    <ListRow>
+                        <TodoItem>TODOです</TodoItem>
+                        <Button>完了</Button>
+                        <Button>削除</Button>
+                    </ListRow>
                 </li>
                 <li>
-                    <p>TODOです</p>
-                    <button>完了</button>
-                    <button>削除</button>
+                    <ListRow>
+                        <TodoItem>TODOです</TodoItem>
+                        <Button>完了</Button>
+                        <Button>削除</Button>
+                    </ListRow>
                 </li>
             </ul>
-        </>
+        </IncompleteArea>
     )
 }
