e32e9b9f92df42944b84aec3b469a6cbdfe48a06
github yamato0142 <yamato.0142tech@gmail.com>
2024-10-27 05:26:14 +0900

    firebaseと紐づけ

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { TodoApp } from "./components/Todo/TodoApp.jsx";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Firebaseアプリの初期化
+const app = initializeApp(firebaseConfig);
+
+// Firebase Authの初期化（必要であれば）
+const auth = getAuth(app);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={`/`} element={<TodoApp></TodoApp>}></Route>
+        <Route path={`/`} element={<TodoApp />} />
       </Routes>
     </BrowserRouter>
   );
